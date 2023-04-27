72c7c7f9a4535f8b1c47068efeb7b6e5f99efe50
SheetalMetiz <129273434+SheetalMetiz@users.noreply.github.com>
2023-04-27 11:50:15 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import { FastifyAdapter } from '@nestjs/platform-fastify';
+
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule, new FastifyAdapter());
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors();
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
